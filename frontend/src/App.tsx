62121d60729849273b4b5a74abebcd6241bf7794
Afonso Cardozo Cruz <afonso.cardozo.cruz@gmail.com>
2022-01-16 18:44:56 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Listing from 'pages/Listing'
+import Form from 'pages/Form'
 import Navbar from 'components/Navbar'
+
+
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form(){
+    return(
+        <>
+        </>
+    );
+}
+
+export default Form;
